3ad946f3027eb448cdf86842ef692a0d6ab86f80
Hieu Nguyen <hieu.trung.nguyen2@vas.edu.vn>
2022-06-29 10:24:21 +0700

    fix favicon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { render } from 'react-dom'
+import Favicon from 'react-favicon'
 import App from './App'
 import { ThemeProvider } from './contexts/theme'
 import './index.css'
 
 
+
 render(
+  
   <ThemeProvider>
+     <Favicon url='https://github.com/hieunguyen0802/hieunguyen0802.github.io/blob/master/favicon.ico' />
     <App />
   </ThemeProvider>,
   document.getElementById('root')
